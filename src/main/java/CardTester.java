42cb49f24f5b7873a139ba9889089cc239d23b68
Jackson Greer <jacksongreer@Marshall-Greers-MacBook-Pro.local>
2020-04-10 16:49:24 -0700

    activity one checkpoint

---
+++ src/main/java/CardTester.java
@@ -0,0 +1,25 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card a = new Card("Two", "Hearts", 2);
+		Card b = new Card("Jack", "Spades", 10);
+		Card c = new Card("Ace", "Diamonds", 11);
+		Card d = new Card("Jack", "Spades", 10);
+
+		System.out.println("The ranks and suits of the cards are as follows...");
+		System.out.println("Card a: " + a.getRank() + " of " + a.getSuit());
+		System.out.println("Card b: " + b.getRank() + " of " + b.getSuit());
+		System.out.println("Card c: " + c.getRank() + " of " + c.getSuit());
+
+		System.out.println("Does Card A equal Card B? " + a.equals(b));
+		System.out.println("Does Card B equal Card D? " + b.equals(d));
+
+	}
+}
